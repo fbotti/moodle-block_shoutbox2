9b2759245322f07003bfffe5f44944966113b8a6
Federico J. Botti <fbotti@gmail.com>
2011-06-14 23:41:08 -0300

    Inicial import of the shoutbox block from http://moodle.org/mod/data/view.php?d=13&rid=2259

---
+++ editdata.php
@@ -0,0 +1,16 @@
+<?php
+require_once('../../config.php');
+global $CFG, $COURSE;
+?>
+
+ <?php
+ $data=$_GET['textarea_val'];
+ $id=$_GET['id'];
+mysql_query("update {$CFG->prefix}block_shoutbox_shoutbox  set data=\"$data\" where id=\"$id\"") or die(mysql_error());
+echo  $id.",";
+echo $data;
+
+
+
+
+?>
